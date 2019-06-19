141edb4982542d5a8c31223918cf1e16d715e5da
Keith Isham <kisham8899@gmail.com>
2019-06-19 14:41:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,9 +65,15 @@ DB[:conn].execute(sql).collect do |row|
 end
 
   def self.first_X_students_in_grade_10
-sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
-    first_student_row = DB[:conn].execute(sql)[0]
-    self.new_from_db(first_student_row)
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE students.grade = 10
+    LIMIT ?
+    SQL
+    DB[:conn].execute(sql)[0].collect do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
