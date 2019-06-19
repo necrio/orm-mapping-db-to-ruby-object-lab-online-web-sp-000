5484dce94cb2de673d4255e5936dbbe642275710
Keith Isham <kisham8899@gmail.com>
2019-06-19 14:35:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,6 +64,15 @@ DB[:conn].execute(sql).collect do |row|
   end
 end
 
+ def self.first_X_students_in_grade_10
+  sql = <<-SQL
+  SELECT *
+  FROM students
+  WHERE students.grade = 10
+  LIMIT 1
+  SQL
+  DB[:conn].execute(sql).collect do |row|
+    self.new_from_db(row)
 
   def save
     sql = <<-SQL
